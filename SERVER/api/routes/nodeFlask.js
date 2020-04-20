d8cc60d18abbd92adf44db775d441ee3c532744a
Janhavi <janhavi.dubule16@siesgst.ac.in>
2020-04-20 22:09:37 +0530

    nodeFlask.js and modelApi.py working proper

--- SERVER/api/routes/nodeFlask.js
+++ SERVER/api/routes/nodeFlask.js
@@ -4,6 +4,7 @@ const mongoClient = require('mongodb').MongoClient;
 const request = require('request-promise');
 const axios = require('axios')
 const request2 = require('request')
+const request3 = require('sync-request')
 const fs = require('fs')
 
 const url = "mongodb+srv://Janhavi:mongodb@projectcluster-azpnv.mongodb.net/test?retryWrites=true&w=majority";
@@ -15,95 +16,40 @@ mongoClient.connect(url, (err,db)=>{
     else{
         const myDB = db.db('ProjectDB')
         const collection = myDB.collection('user')
-        router.get('/:email', function(req,res){
+        router.get('/:email',function(req,response){
             const query = {email : req.params.email}
             collection.find(query).toArray(function(err,result){
                 if(err) throw err;
                 const nodeToFlask = {
-                    calorieCount : result[0].calorieCount
-                }
+                        calorieCount : result[0].calorieCount
+                    }
                 var data = JSON.stringify(nodeToFlask)
-                fs.writeFileSync('dataToMl.json',data)
+                fs.writeFileSync('C:/Users/Janhavi Dubule/Desktop/BE Project/getFit/models/dataToMl.json',data)
+                try{
+                    axios
+                    .post('http://127.0.0.1:5000/nodeFlask', nodeToFlask)
+                    .then(res => {
+                        console.log(`statusCode: ${res.statusCode}`)
+                        // console.log(res)
+                        var returndata = fs.readFileSync("C:/Users/Janhavi Dubule/Desktop/BE Project/getFit/models/recommendation.json");
+                        var recommended_meals = JSON.parse(returndata);
+                        //C:/Users/Janhavi Dubule/Desktop/BE Project/getFit/SERVER/api/recommendation.json
+                        //C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/api/recommendation.json
+                        console.log(recommended_meals)
+                        response.status(200).send(JSON.stringify(recommended_meals))
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                }
+                catch(error){
+                    console.log(error)
+                }
 
-                axios
-                .post('http://127.0.0.1:5000/nodeFlask'), nodeToFlask)
-                .then(res => {
-                    console.log(`statusCode: ${res.statusCode}`)
-                    console.log(res)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                var returndata = fs.readFileSync("C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/api/recommendation.json");
-                var recommended_meals = JSON.parse(returndata);
-                //C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/api/recommendation.json
-                console.log(recommended_meals)
-                res.status(200).send(JSON.stringify(recommended_meals))
-            })
-        }
+        })
+        })
     }
 })
 
 
-                // request2.post(
-                //     'http://127.0.0.1:5000/nodeFlask',
-                //     {
-                //         json : {
-                //             calorieCount : result[0].calorieCount
-                //         }
-                        
-                //     },
-                // (err, res, parsedBody) => {
-                //     if(err) {
-                //         console.error(err)
-                //         return 
-                //     }
-                //     console.log(parsedBody)
-                //     console.log("Printing parsedBody")
-                // })
-                
-                
-            
-
-   
-        
-                
-
-                
-
-                // var options = {
-                //     method: 'POST',
-                //     uri: 'http://127.0.0.1:5000/nodeFlask',
-                //     body: nodeToFlask,
-                //     json: true // Automatically stringifies the body to JSON
-                // };
-
-                // var returndata;
-                // var recommended_meals;
-                // var sendrequest = request(options,  function (error, response, parsedBody) {
-                //     if(error) throw error
-                //     // console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
-                //     returndata = parsedBody;
-                //     console.log("This is Parsed Body")
-                //     console.log(returndata)
-                //     returndata = fs.readFileSync("C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/api/recommendation.json");
-                //     recommended_meals = JSON.parse(returndata);
-                //     //C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/api/recommendation.json
-                //     console.log(recommended_meals)
-                //     res.status(200).send(JSON.stringify(recommended_meals))
-                //     // do something with this data, here I'm assigning it to a variable.
-                
-                   
-                // })
-                // .catch(function (err) {
-                //     console.log(err);
-                // });
-                
-
-            // })
-          
-        // })
-
-//     }
-// })
 module.exports = router;
---
+++ models/modelApi.py
@@ -0,0 +1,58 @@
+from subprocess import call
+import numpy as np
+import sys
+import subprocess
+from flask import Flask, request, jsonify, render_template
+import json
+#import pickle
+
+app = Flask(__name__)
+
+#model2 = pickle.load(open("C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/myfile2.pkl",'rb'))
+
+
+
+
+class CallPy(object):
+
+    def __init__(self, path = 'C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/models/be_proj_ml_algo.py'):
+        self.path = path
+    
+    def call_algo(self):
+        call(["Python3", "{}".format(self.path)])
+
+    
+
+@app.route('/')
+def home():
+    return "Flask server"
+
+
+@app.route('/nodeFlask', methods = ['POST'])
+def meal_predict():
+
+    # call(["Python3", "{}".format('C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/models/be_proj_ml_algo.py')])
+    s2_out = subprocess.check_output([sys.executable, "C:/Users/Janhavi Dubule/Desktop/BE Project/getFit/models/be_proj_ml_algo.py"])
+
+    #C:/Users/kkatt/Documents/BE_Project/getFit/SERVER/meal.json
+    #C:/Users/Janhavi Dubule/Desktop/BE Project/getFit/SERVER/meal.json
+    with open("C:/Users/Janhavi Dubule/Desktop/BE Project/getFit/models/meal.json") as f2:
+        meals = json.load(f2)
+    
+    #C:/Users/Janhavi Dubule/Desktop/BE Project/getFit//SERVER/api/recommendation.json
+    out_file = open("C:/Users/Janhavi Dubule/Desktop/BE Project/getFit//models/recommendation.json", "w") 
+    json.dump(meals, out_file, indent = 2)
+    out_file.close() 
+    print("printing s2:",s2_out)
+    return "Returning meal recommendation"
+
+    #return json.dumps("new data" : "Ml algorithm output")
+
+#meal_predict()
+
+
+
+if __name__ == "__main__":
+    c = CallPy()
+    c.call_algo()
+    app.run(debug=True)
